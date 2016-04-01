b2e38518e77571ec12a691daa017531271f618f2
Nat Weerawan <nat.wrw@gmail.com>
2016-04-01 11:05:56 +0700

    Update examples.

--- examples/interval/interval.ino
+++ examples/interval/interval.ino
@@ -1,29 +1,15 @@
-#include "CMMCInterval.h"
+#include <Arduino.h>
+#include "CMMCEasy.h"
 
-void HelloWorld();
-void LoveYou();
-
-CMMCInterval myInterval1(1000, HelloWorld);
-CMMCInterval myInterval2(500, LoveYou);
+CMMCEasy easy;
+CMMC_Interval &interval = easy.interval;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  myInterval1.Update();
-  myInterval2.Update();
+  interval.every_ms(301, [&]() {
+    Serial.printf("M = %lu\n", millis());
+  });
 }
-
-void HelloWorld()
-{
-    Serial.println("Helloworld");
-}
-
-void LoveYou()
-{
-    Serial.println("LoveYou");
-}
-
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
 #include "../CMMCEasy.h"
-#include "../CMMC_Interval.hpp"
 CMMCEasy easy;
 CMMC_Interval ti;
 
@@ -11,31 +10,32 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
 
+  easy.blinker.init(CMMC_Blink::TYPE_TICKER);
   easy.blinker.blink(500, LED_BUILTIN);
   delay(3000);
-  // easy.blinker.blink(20, LED_BUILTIN);
+  easy.blinker.blink(20, LED_BUILTIN);
   delay(2000);
-  // easy.blinker.blink(500, LED_BUILTIN);
-  // delay(5000);
-  // easy.blinker.blink(100, LED_BUILTIN);
-  // delay(1000);
-  // easy.blinker.blink(1000);
-  // delay(1000);
-  // easy.blinker.blink(100);
+  easy.blinker.blink(500, LED_BUILTIN);
+  delay(1000);
+  easy.blinker.blink(100, LED_BUILTIN)  ;
+  delay(1000);
+  easy.blinker.blink(1000);
+  delay(1000);
+  easy.blinker.blink(100);
 
-}
+  }
 
 
 void loop() {
   int outside = 0;
   static int outside_static = 0;
 
-  ti.every_ms(222, [&]() {
-    // outside++;
-    // outside_static++;
-    // Serial.printf("IN SIDE %lu \n", millis());
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  });
+  // ti.every_ms(222, [&]() {
+  //   // outside++;
+  //   // outside_static++;
+  //   // Serial.printf("IN SIDE %lu \n", millis());
+  //   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  // });
 
   // delay(1000);
   // Serial.printf("outside = %d, outside_static = %d\n", outside, outside_static);
